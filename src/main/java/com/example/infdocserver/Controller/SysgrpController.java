26764a6db125ce4164cc345b64a517543e2ef64f
aion0721 <kobayashi@test.com>
2020-05-17 18:06:14 +0900

    Create create endpoint.

--- src/main/java/com/example/infdocserver/Controller/SysgrpController.java
+++ src/main/java/com/example/infdocserver/Controller/SysgrpController.java
@@ -2,9 +2,13 @@ package com.example.infdocserver.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 
 import com.example.infdocserver.Domain.Sysgrp;
 import com.example.infdocserver.Service.SysgrpService;
@@ -21,6 +25,13 @@ public class SysgrpController {
     @GetMapping("list")
     public List<Sysgrp> list(@ModelAttribute Sysgrp sysgrp) {
         return sysgrpService.findAll();
+    }
+
+    @PostMapping("create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sysgrp create(@RequestBody Sysgrp sysgrp) {
+        sysgrpService.save(sysgrp);
+        return sysgrp;
     }
 
 }
