6347f12fbc2176879e4596d2aa1c4e85bd70c50d
aion0721 <rudolf.aion@gmail.com>
2020-08-18 08:39:09 +0900

    Jcip sample / SynchronizedFactorizer

--- src/main/java/com/example/infdocserver/UnsafeCountingFactorizer.java
+++ src/main/java/com/example/infdocserver/UnsafeCountingFactorizer.java
@@ -5,23 +5,34 @@ import javax.servlet.*;
 
 import net.jcip.annotations.*;
 
-/**
- * UnsafeCountingFactorizer
- *
- * Servlet that counts requests without the necessary synchronization
- *
- * @author Brian Goetz and Tim Peierls
- */
-@NotThreadSafe
-public class LazyInitRace {
-    private ExpensiveObject instance = null;
+@ThreadSafe
+public class SynchronizedFactorizer extends GenericServlet implements Servlet {
+    @GuardedBy("this")
+    private BigInteger lastNumber;
+    @GuardedBy("this")
+    private BigInteger[] lastFactors;
 
-    public ExpensiveObject getInstance() {
-        if (instance == null)
-            instance = new ExpensiveObject();
-        return instance;
+    public synchronized void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        if (i.equals(lastNumber))
+            encodeIntoResponse(resp, lastFactors);
+        else {
+            BigInteger[] factors = factor(i);
+            lastNumber = i;
+            lastFactors = factors;
+            encodeIntoResponse(resp, factors);
+        }
     }
-}
 
-class ExpensiveObject {
+    void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+    }
+
+    BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");
+    }
+
+    BigInteger[] factor(BigInteger i) {
+        // Doesn't really factor
+        return new BigInteger[] { i };
+    }
 }
