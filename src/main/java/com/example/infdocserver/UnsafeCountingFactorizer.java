bbb48435f6e7007db88848f94354fe20fa52f23d
aion0721 <rudolf.aion@gmail.com>
2020-08-18 08:29:43 +0900

    Jcip sample / LazyInitRace

---
+++ src/main/java/com/example/infdocserver/UnsafeCountingFactorizer.java
@@ -0,0 +1,27 @@
+package com.example.infdocserver;
+
+import java.math.BigInteger;
+import javax.servlet.*;
+
+import net.jcip.annotations.*;
+
+/**
+ * UnsafeCountingFactorizer
+ *
+ * Servlet that counts requests without the necessary synchronization
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@NotThreadSafe
+public class LazyInitRace {
+    private ExpensiveObject instance = null;
+
+    public ExpensiveObject getInstance() {
+        if (instance == null)
+            instance = new ExpensiveObject();
+        return instance;
+    }
+}
+
+class ExpensiveObject {
+}
